f261c057ca0b4b820ad76284df62ba5f142132ce
umar-io <mailpersonal725@gmail.com>
2024-11-26 09:57:21 +0100

    nomessage

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -19,12 +19,12 @@ const Footer = () => {
   ]
   const year = new Date().getFullYear();
   return (
-    <footer className="footer w-full flex justify-between items-center p-6 lg:py-4 lg:px-[50px] bottom-0">
-      <p className="lg:text-[20px] font-thin text-[12px] text-white"> &copy; {year} Umar Irale</p>
+    <footer className="footer w-full flex justify-between items-center p-6 lg:py-4 lg:px-[50px] bottom-0 border-t-black border">
+      <p className="lg:text-[20px] font-thin text-[12px] text-[#1F271B]"> &copy; {year} Umar Irale</p>
       <article className="flex gap-8">
         {
           footerLink.map((footerlink, index)=>(
-           <Link to={footerlink.link} key={index} className="lg:text-2xl text-[18px] text-white">
+           <Link to={footerlink.link} key={index} className="lg:text-2xl text-[18px] text-[#1F271B]">
               {footerlink.icon}
            </Link>
           ))
